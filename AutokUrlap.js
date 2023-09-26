848700fcc7c8f93b04e05e05d35f25b335005c19
fureszb <fureszb@gmail.com>
2023-09-26 09:42:28 +0200

    construktor_vlatoztatva

--- AutokUrlap.js
+++ AutokUrlap.js
@@ -1,11 +1,10 @@
-class AutokUrlap{
-    #obj={
-        tipus: "",
-        rendszam: "",
-    }
-    constructor(obj, szuloElem){
-        this.#obj = obj;
-        this.szuloElem = szuloElem.append(`<h2>Autók űrlap</h2>
+class AutokUrlap {
+  #obj = {
+    tipus: "",
+    rendszam: "",
+  };
+  constructor(szuloElem) {
+    this.szuloElem = szuloElem.append(`<h2>Autók űrlap</h2>
         <form>
           <div class="form-group">
             <label for="tipus">Tipus:</label>
@@ -15,23 +14,27 @@ class AutokUrlap{
             <label for="rendszam">Rendszám:</label>
             <input type="text" class="form-control" id="rendszam" placeholder="Írd be az autód rendszámát" name="rendszam">
           </div>
-          <button type="submit" class="btn btn-default kuld">Elküldés</button>
+          <button type="button" class="btn btn-success kuld">Elküldés</button>
         </form>`);
-        this.tipus = $("#tipus");
-        this.rendszam = $("#rendszam");
-        this.sumbit = $(".kuld");
-        this.sumbit.on("click", ()=>{
-            event.preventDefault();
-            this.#obj.tipus = this.tipus.val();
-            this.#obj.rendszam = this.rendszam.val();
-            this.#esemenyTrigger("hozzaad");
-            this.tipus.val('');
-            this.rendszam.val('');
-        });
-    }
-    #esemenyTrigger(nev){
-        const esemeny = new CustomEvent(nev, {detail : this.#obj});
-        window.dispatchEvent(esemeny);
-    }
+    this.tipus = $("#tipus");
+    this.rendszam = $("#rendszam");
+    this.sumbit = $(".kuld");
+    this.sumbit.on("click", (event) => {
+      event.preventDefault();
+
+      this.#obj.tipus = this.tipus.val();
+      this.#obj.rendszam = this.rendszam.val();
+
+      this.#esemenyTrigger("hozzaad");
+      this.tipus.val("");
+      this.rendszam.val("");
+    });
+  }
+  #esemenyTrigger(nev) {
+    //console.log(this.#obj);
+
+    const esemeny = new CustomEvent(nev, { detail: this.#obj });
+    window.dispatchEvent(esemeny);
+  }
 }
 export default AutokUrlap;
--- main.js
+++ main.js
@@ -3,9 +3,10 @@ import { AUTO } from "./adatok.js";
 $(function(){
     let szuloElem = $(".taroloForm");
     
-    new AutokUrlap(AUTO, szuloElem)
+    new AutokUrlap(szuloElem)
     
     $(window).on("hozzaad", (event)=>{
+        //console.log(event.detail)
         AUTO.push(event.detail);
         console.log(AUTO);
     });
